d01ffab57966d8ef1393759d021b5882207d31f5
marcel <marcel@marcel-laptop.(none)>
2010-06-02 09:50:10 -0300

    unit test torniera dispatcher

---
+++ tests/unit/core/test_dispatcher.py
@@ -0,0 +1,14 @@
+from mox import Mox
+from torneira.core import dispatcher
+
+def test_can_be_load_dsipatcher():
+	mox = Mox()
+
+	mox.ReplayAll()
+	try:
+		_dispatcher = dispatcher.TorneiraDispatcher()
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
+	
+	
