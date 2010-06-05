d0f251fee061ddd71fc28a75680b1da20268c4ff
marcel <marcel@marcel-laptop.(none)>
2010-06-04 21:06:30 -0300

    test server render templates

--- tests/unit/core/test_server.py
+++ tests/unit/core/test_server.py
@@ -1,5 +1,7 @@
 from mox import Mox
 from torneira.core import server
+from tornado.web import HTTPError
+from nose.tools import assert_equals, assert_raises
 
 def test_can_be_load_server():
 	_server = server.TorneiraServer("pidfile", 8080, "project_root", "media_dir")
@@ -199,6 +201,173 @@ def test_can_handler_process_request_match():
 	finally:
 		mox.UnsetStubs()
 	
+def test_can_handler_process_request_raise500():
+	mox = Mox()
 	
+	mox = Mox()
+
+	mox.StubOutWithMock(server, "TorneiraDispatcher", use_mock_anything=True)
+
+	application_mock = mox.CreateMockAnything()
+	application_mock.ui_methods = {}
+	application_mock.ui_modules = {}	
 	
+	request_mock = mox.CreateMockAnything()
+	request_mock.uri = "/should-be-uri.html"
+	request_mock.supports_http_1_1().AndReturn(True)
 	
+	match_value = {'controller':'should-be-controller','action':'shouldBeAction'}
+	
+	prepared_arguments_mock = mox.CreateMockAnything()
+	prepared_arguments_mock(match_value).AndReturn({'arg':'value'})
+	
+	mapper_mock = mox.CreateMockAnything()
+	mapper_mock.match("/should-be-uri.html").AndReturn(match_value)
+	
+	controller_mock = mox.CreateMockAnything()
+	controller_mock.shouldBeAction(arg='value').AndRaise(Exception)
+	
+	dispatcher_mock = mox.CreateMockAnything()
+	dispatcher_mock.getMapper().AndReturn(mapper_mock)
+	dispatcher_mock.getController('should-be-controller').AndReturn(controller_mock)
+	
+	server.TorneiraDispatcher().AndReturn(dispatcher_mock)
+	server.TorneiraDispatcher().AndReturn(dispatcher_mock)
+	
+	mox.ReplayAll()
+
+	try:
+		handler = server.TorneiraHandler(application_mock, request_mock)
+		handler.prepared_arguments = prepared_arguments_mock
+		assert_raises(HTTPError, handler.process_request)
+			
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
+
+def test_can_handler_process_request_render_500_template():
+	mox = Mox()
+
+	mox.StubOutWithMock(server, "TorneiraDispatcher", use_mock_anything=True)
+	mox.StubOutWithMock(server, "BaseController", use_mock_anything=True)
+	mox.StubOutWithMock(server, "settings", use_mock_anything=True)
+	
+	server.settings.DEBUG = False
+
+	application_mock = mox.CreateMockAnything()
+	application_mock.ui_methods = {}
+	application_mock.ui_modules = {}	
+	
+	request_mock = mox.CreateMockAnything()
+	request_mock.uri = "/should-be-uri.html"
+	request_mock.supports_http_1_1().AndReturn(True)
+	
+	match_value = {'controller':'should-be-controller','action':'shouldBeAction'}
+	
+	prepared_arguments_mock = mox.CreateMockAnything()
+	prepared_arguments_mock(match_value).AndReturn({'arg':'value'})
+	
+	mapper_mock = mox.CreateMockAnything()
+	mapper_mock.match("/should-be-uri.html").AndReturn(match_value)
+	
+	controller_mock = mox.CreateMockAnything()
+	controller_mock.shouldBeAction(arg='value').AndRaise(Exception)
+	
+	dispatcher_mock = mox.CreateMockAnything()
+	dispatcher_mock.getMapper().AndReturn(mapper_mock)
+	dispatcher_mock.getController('should-be-controller').AndReturn(controller_mock)
+	
+	server.TorneiraDispatcher().AndReturn(dispatcher_mock)
+	server.TorneiraDispatcher().AndReturn(dispatcher_mock)
+	
+	base_controller_mock = mox.CreateMockAnything()
+	base_controller_mock.render_to_template("/500.html").AndReturn("should-be-500-response")
+	server.BaseController().AndReturn(base_controller_mock)
+	write_mock = mox.CreateMockAnything()
+	write_mock("should-be-500-response")
+
+
+	mox.ReplayAll()
+
+	try:
+		handler = server.TorneiraHandler(application_mock, request_mock)
+		handler.prepared_arguments = prepared_arguments_mock
+		handler.write = write_mock
+		handler.process_request()
+			
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
+
+def test_can_handler_process_request_raise404():
+	mox = Mox()
+
+	mox.StubOutWithMock(server, "TorneiraDispatcher", use_mock_anything=True)
+
+	application_mock = mox.CreateMockAnything()
+	application_mock.ui_methods = {}
+	application_mock.ui_modules = {}	
+	
+	request_mock = mox.CreateMockAnything()
+	request_mock.uri = "/should-be-uri.html"
+	request_mock.supports_http_1_1().AndReturn(True)
+	
+	mapper_mock = mox.CreateMockAnything()
+	mapper_mock.match("/should-be-uri.html").AndReturn(False)
+	
+	dispatcher_mock = mox.CreateMockAnything()
+	dispatcher_mock.getMapper().AndReturn(mapper_mock)
+	
+	server.TorneiraDispatcher().AndReturn(dispatcher_mock)
+	
+	mox.ReplayAll()
+
+	try:
+		handler = server.TorneiraHandler(application_mock, request_mock)
+		assert_raises(HTTPError, handler.process_request)
+			
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
+
+def test_can_handler_process_request_render_404_template():
+	mox = Mox()
+
+	mox.StubOutWithMock(server, "TorneiraDispatcher", use_mock_anything=True)
+	mox.StubOutWithMock(server, "BaseController", use_mock_anything=True)
+	mox.StubOutWithMock(server, "settings", use_mock_anything=True)
+	
+	server.settings.DEBUG = False
+	
+	application_mock = mox.CreateMockAnything()
+	application_mock.ui_methods = {}
+	application_mock.ui_modules = {}	
+	
+	request_mock = mox.CreateMockAnything()
+	request_mock.uri = "/should-be-uri.html"
+	request_mock.supports_http_1_1().AndReturn(True)
+	
+	mapper_mock = mox.CreateMockAnything()
+	mapper_mock.match("/should-be-uri.html").AndReturn(False)
+	
+	dispatcher_mock = mox.CreateMockAnything()
+	dispatcher_mock.getMapper().AndReturn(mapper_mock)
+	
+	server.TorneiraDispatcher().AndReturn(dispatcher_mock)
+	
+	base_controller_mock = mox.CreateMockAnything()
+	base_controller_mock.render_to_template("/404.html").AndReturn("should-be-404-response")
+	server.BaseController().AndReturn(base_controller_mock)
+	write_mock = mox.CreateMockAnything()
+	write_mock("should-be-404-response")
+
+	mox.ReplayAll()
+
+	try:
+		handler = server.TorneiraHandler(application_mock, request_mock)
+		handler.write = write_mock
+		handler.process_request()
+			
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
