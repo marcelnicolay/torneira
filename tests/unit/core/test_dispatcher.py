179370d00b91ce76d616d5bdfa4a6d7fb4395c42
marcel <marcel@marcel-laptop.(none)>
2010-06-20 13:16:47 -0300

    test dispatcher urls and mappers

--- tests/unit/core/test_dispatcher.py
+++ tests/unit/core/test_dispatcher.py
@@ -1,12 +1,51 @@
 from mox import Mox
 from torneira.core import dispatcher
 
-def test_can_be_load_dsipatcher():
+urls = [("url_name", "url_route", "controller", "action", "module")]
+
+def test_can_be_load_dispatcher():
 	mox = Mox()
 
 	mox.ReplayAll()
 	try:
 		_dispatcher = dispatcher.TorneiraDispatcher()
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
+
+def test_can_be_get_urls():
+	mox = Mox()
+
+	mox.StubOutWithMock(dispatcher, "settings", use_mock_anything=True)
+	dispatcher.settings.ROOT_URLS = 'unit.core.test_dispatcher'
+	
+	_dispatcher = dispatcher.TorneiraDispatcher()
+	
+	mox.ReplayAll()
+	try:
+		assert urls == _dispatcher.getUrls()
+		mox.VerifyAll()
+	finally:
+		mox.UnsetStubs()
+
+def test_can_be_get_mapper():
+	mox = Mox()
+
+	mox.StubOutWithMock(dispatcher, "settings", use_mock_anything=True)
+	mox.StubOutWithMock(dispatcher, "Mapper", use_mock_anything=True)
+	
+	dispatcher.settings.ROOT_URLS = 'unit.core.test_dispatcher'
+	
+	mapper_mock = mox.CreateMockAnything()
+	mapper_mock.connect('url_name', 'url_route', action='action', controller='module.controller')
+	
+	dispatcher.Mapper().AndReturn(mapper_mock)
+	
+	_dispatcher = dispatcher.TorneiraDispatcher()
+	
+	mox.ReplayAll()
+	try:
+		assert mapper_mock == _dispatcher.getMapper()
 		mox.VerifyAll()
 	finally:
 		mox.UnsetStubs()
