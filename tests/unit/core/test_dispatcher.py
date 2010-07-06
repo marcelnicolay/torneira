7f0cfff896614d433e16e3420c4e0b895f7ed8e5
Rodrigo Machado <rcmachado@gmail.com>
2010-07-06 09:59:31 -0300

    Add license header to files
    Add missing license (OSL v3.0) disclaimer on source files

--- tests/unit/core/test_dispatcher.py
+++ tests/unit/core/test_dispatcher.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Open Software License ("OSL") v. 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.opensource.org/licenses/osl-3.0.php
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from mox import Mox
 from torneira.core import dispatcher
 
@@ -18,9 +32,9 @@ def test_can_be_get_urls():
 
 	mox.StubOutWithMock(dispatcher, "settings", use_mock_anything=True)
 	dispatcher.settings.ROOT_URLS = 'unit.core.test_dispatcher'
-	
+
 	_dispatcher = dispatcher.TorneiraDispatcher()
-	
+
 	mox.ReplayAll()
 	try:
 		assert urls == _dispatcher.getUrls()
@@ -33,16 +47,16 @@ def test_can_be_get_mapper():
 
 	mox.StubOutWithMock(dispatcher, "settings", use_mock_anything=True)
 	mox.StubOutWithMock(dispatcher, "Mapper", use_mock_anything=True)
-	
+
 	dispatcher.settings.ROOT_URLS = 'unit.core.test_dispatcher'
-	
+
 	mapper_mock = mox.CreateMockAnything()
 	mapper_mock.connect('url_name', 'url_route', action='action', controller='module.controller')
-	
+
 	dispatcher.Mapper().AndReturn(mapper_mock)
-	
+
 	_dispatcher = dispatcher.TorneiraDispatcher()
-	
+
 	mox.ReplayAll()
 	try:
 		assert mapper_mock == _dispatcher.getMapper()
--- torneira/helper/simplexml.py
+++ torneira/helper/simplexml.py
@@ -1,12 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Licensed under the Open Software License ("OSL") v. 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.opensource.org/licenses/osl-3.0.php
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from xml.dom.minidom import getDOMImplementation
 
 
 def element_from_dict(document, elRoot, data):
 
 	for k, v in data.iteritems():
-		
+
 		elem = document.createElement(k)
-		
+
 		if isinstance(v, dict):
 			element_from_dict(document, elem, v)
 		elif isinstance(v, list):
@@ -19,17 +33,17 @@ def element_from_dict(document, elRoot, data):
 			elem.appendChild(document.createCDATASection(v))
 		else:
 			elem.appendChild(document.createTextNode(str(v)))
-						
+
 		elRoot.appendChild(elem)
-		
+
 def dumps(data):
-	
+
 	rootName, rootValue = data.items()[0]
 	implementation = getDOMImplementation()
 	document = implementation.createDocument(None, rootName, None)
-	
+
 	rootNode = document.documentElement
-	
+
 	element_from_dict(document, rootNode,  rootValue)
-				
+
 	return document.toxml()
