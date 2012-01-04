d0e3f0846852207a2776efc0ba11476aff353132
marcel <marcel.nicolay@gmail.com>
2012-01-04 17:30:39 -0200

    fix test case constructor

--- torneira/testing/testcase.py
+++ torneira/testing/testcase.py
@@ -3,5 +3,8 @@ from torneira.testing.client import TestingClient
 
 class TestCase(unittest2.TestCase):
     
-    def __init__(self):
+    def __init__(self, *args, **kargs):
+
         self.client = TestingClient()
+        
+        super(TestCase, self).__init__(*args, **kargs)
