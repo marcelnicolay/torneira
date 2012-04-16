d9f1286b9394d758c2a718241e9ee1e395e1e3c7
Gustavo Barbosa <gustavocsb@gmail.com>
2012-04-16 18:02:13 -0300

    Bump to 0.2.8

--- torneira/__init__.py
+++ torneira/__init__.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 
 try:
     import settings_local as settings
