895d7088d2ed06dc223cd87284b84babaf25e4ef
Rodrigo Machado <rcmachado@gmail.com>
2012-04-20 20:39:45 -0300

    Fix incorrect test result with hash randomization activated

--- tests/unit/testing/test_client.py
+++ tests/unit/testing/test_client.py
@@ -3,7 +3,7 @@ import unittest2
 from torneira.testing.client import TestingClient
 
 class TestingClientTestCase(unittest2.TestCase):
-    def test_can_convert_dict_wtih_list_to_post_data(self):
+    def test_can_convert_dict_with_list_to_post_data(self):
         data = {
             'name': 'should be name',
             'status': 1,
@@ -11,5 +11,8 @@ class TestingClientTestCase(unittest2.TestCase):
         }
 
         parsed_data = TestingClient.parse_post_data(data)
-        expected_data = 'status=1&itens=1&itens=4&itens=3&name=should+be+name'
-        self.assertEquals(parsed_data, expected_data)
+        self.assertTrue('status=1' in parsed_data)
+        self.assertTrue('itens=1' in parsed_data)
+        self.assertTrue('itens=4' in parsed_data)
+        self.assertTrue('itens=3' in parsed_data)
+        self.assertTrue('name=should+be+name' in parsed_data)
