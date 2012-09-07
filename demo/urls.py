d49c85a4c40ea6d1c8a0282312133a21868bc5ce
Rodrigo Machado <rcmachado@gmail.com>
2012-09-07 13:00:49 -0300

    Remove legacy demo app

--- demo/urls.py
+++
@@ -1,9 +0,0 @@
-from handlers.home import HomeHandler
-from tornado.web import url
-
-
-urls = (
-    url(r"/custom/?", HomeHandler, {'action': 'custom'}),
-    url(r"/json_service/?", HomeHandler, {'action': 'json_service'}),
-    url(r"/.*", HomeHandler, {'action': 'index'}),
-)
